ae785af1884fab82510cf1240324977956a1c9bb
Vladimir Makeev <makeevvolodja@gmail.com>
2018-10-22 20:11:24 +0300

    really disable pretty input printing

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	size_t commands_per_block = std::stoull(argv[1]);
-	const bool PRINT_INPUT = true;
+	const bool PRINT_INPUT = false;
 
 	std::unique_ptr<otus::IFileLogger> file_logger = std::make_unique<otus::FileLogger>();
 	std::unique_ptr<otus::IReader> reader = std::make_unique<otus::Reader>(std::cin);
