4a0127d18e018cc9d987dd59654e97bbae860a4b
Vladimir Makeev <makeevvolodja@gmail.com>
2018-12-07 22:14:41 +0300

    allow empty line as command

--- src/io/reader.cpp
+++ src/io/reader.cpp
@@ -1,16 +1,13 @@
 #include "reader.h"
 
 namespace otus {
-	Reader::Reader(std::istream &istream)
-		: istream_(istream) {}
+  Reader::Reader(std::istream &istream)
+      : istream_(istream) {}
 
-	void Reader::start_reading_cycle() {
-		while (istream_) {
-			std::string tmp;
-			std::getline(istream_, tmp);
-			if (!tmp.empty()) {
-				notify_all(tmp);
-			}
-		}
-	}
+  void Reader::start_reading_cycle() {
+      std::string tmp;
+      while (std::getline(istream_, tmp)) {
+          notify_all(tmp);
+      }
+  }
 } // namespace otus
